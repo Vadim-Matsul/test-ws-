8b2dea03f162d5a60081e0c84f88bef0d2038b8b
Vadim <Matsulmoscow@gmail.com>
2022-07-04 14:42:28 +0300

    del import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './components/AppRouter';
-import Posts from './pages/Posts';
 import './styles/App.css';
-
-
-
 
 
 function App() {
