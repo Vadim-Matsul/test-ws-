95bfcfc8246dbb9d11ac8cced3ed1b23013c734b
Vadim <Matsulmoscow@gmail.com>
2022-07-07 16:30:40 +0300

    i am updated autorization form and added some hints

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -3,6 +3,7 @@ import MyButton from "../components/UI/button/MyButton";
 import MyInput from "../components/UI/input/MyInput";
 import Popup from "../components/UI/popup/Popup";
 import { AppContext } from "../context";
+import { AuthError } from "../utils/AuthError";
 
 const AuthPage = () => {
  
@@ -10,29 +11,105 @@ const AuthPage = () => {
  const [userInfo, setUserInfo] = useState(
     {name: '', password: '', error: ''}
  )
+ const UserName = userInfo.name.length
+ const UserPass = userInfo.password.length
+
  const config = {
-    error_p_l: 'Пустые поля'
+          error_p_l: 'Введите логин и пароль',
+            error_l: 'Введите логин',
+            error_p: 'Введите пароль',
+      error_l_short: 'Короткий логин',
+      error_p_short: 'Короткий пароль',
+       error_l_long: 'Длинный логин',
+       error_p_long: 'Длинный пароль',
+      err_l_p_short: 'Короткий логин и пароль',
+       err_l_p_long: 'Длинный логин и пароль',
+ err_l_long_p_short: 'Длинный логин и короткий пароль',
+ err_p_long_l_short: 'Длинный пароль и короткий логин',
+ 
+     userData: {
+        setAuth: setAuth,
+        userInfo: userInfo,
+        setUserInfo: setUserInfo
+    }
  }
- const blockError = document.querySelector('.AuthPage-Error')
 
 
-const logSystem = (event) => {
+
+ const logSystem = (event) => {
     event.preventDefault()
-    if (userInfo.name.length && userInfo.password.length){
-        if (userInfo.name.length <= 12  ){
+
+    if (UserName || UserPass){
+      if ( UserName > 3 
+           && UserName <= 12 
+           &&  UserPass > 8 
+           && UserPass <= 30 ){
             localStorage.setItem('auth', 'true')
             localStorage.setItem('userName', userInfo.name)
+            localStorage.setItem('userPassword', userInfo.password)
             setAuth (true)
-        }
+        } 
+      if ( ( UserPass <= 8 || UserPass > 30 ) 
+             && UserName > 3 
+             && UserName <= 12 
+             && UserPass !== 0 ){
+                if (UserPass <= 8){ AuthError (config.userData, config.error_p_short) }
+                if (UserPass > 30){ AuthError (config.userData, config.error_p_long ) } 
+      }
+      if ( ( UserName <= 3 || UserName > 12 ) 
+             && UserPass > 8 
+             && UserPass <= 30 
+             && UserName !==0 ){
+                if (UserName <= 3){ AuthError (config.userData, config.error_l_short) }
+                if (UserName > 12){ AuthError (config.userData, config.error_l_long ) }
+      }
+      if ( UserName > 12 
+           && UserPass > 30 ) {
+        AuthError (config.userData, config.err_l_p_long)
+      }
+      if ( UserName <= 3 
+           && UserPass <= 8 
+           && UserName !== 0 
+           && UserPass !==0 ) {
+        AuthError (config.userData, config.err_l_p_short)
+      }
+      if ( UserName > 3 
+           && UserName <= 12 
+           && !UserPass ){
+        AuthError (config.userData, config.error_p)
+      }
+      if ( ( UserName <= 3 || UserName > 12 ) 
+             && !UserPass  ){
+                if (UserName <= 3){ AuthError (config.userData, config.error_l_short) }
+                if (UserName > 12){ AuthError (config.userData, config.error_l_long ) }
+      }
+      if ( UserPass > 8 
+           && UserPass <= 30 
+           && !UserName ){
+        AuthError (config.userData, config.error_l)
+      }
+      if ( ( UserPass <= 8 || UserPass > 30 ) 
+             && !UserName ){
+                if (UserPass <= 8){ AuthError (config.userData, config.error_p_short) }
+                if (UserPass > 30){ AuthError (config.userData, config.error_p_long ) }
+      }
+      if ( UserPass > 30 
+           && UserName <= 3 
+           && UserName !==0 ){
+        AuthError (config.userData, config.err_p_long_l_short)
+      }
+      if ( UserName > 12 
+           && UserPass <= 8 
+           && UserPass !==0 ){
+        AuthError (config.userData, config.err_l_long_p_short)
+      }
     } else {
-        setUserInfo ({...userInfo, error: config.error_p_l})
-        if(blockError){
-            blockError.classList.toggle('AuthPage-Error-Anim')
-        }
-        
-       } 
-   
+        AuthError (config.userData, config.error_p_l)
+    } 
 }
+
+
+
     return (
         <div>
           <div className={'Log'}>
@@ -54,7 +131,7 @@ const logSystem = (event) => {
                     placeholder='Пароль'
                     type='password'
                     />
-                <MyButton onClick={logSystem}> Войти </MyButton>
+                <MyButton > Войти </MyButton>
                 </form>
             </div>
           </div>
---
+++ src/utils/AuthError.js
@@ -0,0 +1,18 @@
+
+
+
+export const AuthError = ({...config}, message) => {
+    const blockError = document.querySelector('.AuthPage-Error')
+
+        if ( blockError.classList.contains('AuthPage-Error-Anim') ){
+               blockError.classList.remove('AuthPage-Error-Anim')
+                setTimeout ( () => {
+                    blockError.classList.add('AuthPage-Error-Anim')
+                }, 0.0001 )
+        } else {
+              blockError.classList.add('AuthPage-Error-Anim')
+        }
+
+      config.setUserInfo ({...config.userInfo, error: message})
+  config.setAuth( false )
+}
